799305b7778bd3a55795cd61af6bf3b2764a992f
rometnaruskberg <91244913+rometnaruskberg@users.noreply.github.com>
2021-10-16 15:09:16 +0300

    Add files via upload

---
+++ task2.1.js
@@ -0,0 +1,35 @@
+const firstName = 'Romet';
+const birthDay = '02';      
+const birthMonth = '10';
+const birthYear = '1993';
+const hobbies = ('reading', 'weightlifting');
+const object = {
+    firstName: firstName,
+    birthDay: birthDay,
+    birthMonth: birthMonth,
+    birthYear: birthYear,
+    hobbies: hobbies
+}
+
+console.log(object)
+
+//{firstName: 'Romet', birthDay: '02', birthMonth: '10', birthYear: '1993', hobbies: 'weightlifting'} Ma ei saa aru miks ta mõlemat hobi välja ei logi.
+
+const firstName2 = 'Romet';
+const bDay = new Date(1993, 11, 02);
+const hobbies2 = ('reading', 'weightlifting');
+const object2 = {
+    firstName2: firstName2,
+    birthYear2: bDay.getFullYear()
+    birthMonth2: bDay.getMonth(),
+    birthDay2: bDay.getDate(),
+    hobbies2: hobbies2
+}
+
+const bDay = object2.birthYear2 + '.' + object2.birthMonth2 + '.' + object2.birthDay2
+
+console.log(birthDate)
+
+// Midagi on koodis viga, kuid ei saa aru mis
+
+
